279f7abd5341dd360cd075e0ec8998b0bfa1d052
JavierMachucaA <javier.machuca.a@gmail.com>
2020-12-13 23:10:48 -0300

    add banner and content

--- front/landingpage/src/app/app.module.ts
+++ front/landingpage/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-import {NgxLocalStorageModule} from 'ngx-localstorage';
+import { NgxLocalStorageModule } from 'ngx-localstorage';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './layout/layout.component';
 import { MenuComponent } from './layout/menu/menu.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,11 @@ import { BrandComponent } from './components/brand/brand.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatDialogModule,
+    MatButtonModule,
     HttpClientModule,
+    MatIconModule,
     CollapseModule.forRoot(),
     NgxLocalStorageModule.forRoot(),
   ],
--- front/landingpage/src/app/components/brand/brand.component.ts
+++ front/landingpage/src/app/components/brand/brand.component.ts
@@ -1,38 +1,51 @@
-import { Component, OnInit,Output,  EventEmitter, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ChangeDetectorRef, Input } from '@angular/core';
 import { Subscription, Observable, timer } from 'rxjs';
 import * as moment from 'moment';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
-  selector: 'brand',
-  templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.scss'],
-  animations: [
-    // the fade-in/fade-out animation.
-    trigger('simpleFadeAnimation', [
+	selector: 'brand',
+	templateUrl: './brand.component.html',
+	styleUrls: ['./brand.component.scss'],
+	animations: [
+		// the fade-in/fade-out animation.
+		trigger('simpleFadeAnimation', [
 
-      // the "in" style determines the "resting" state of the element when it is visible.
-      state('in', style({opacity: 1})),
+			// the "in" style determines the "resting" state of the element when it is visible.
+			state('in', style({ opacity: 1 })),
 
-      // fade in when created. this could also be written as transition('void => *')
-      transition(':enter', [
-        style({opacity: 0}),
-        animate(600 )
-      ]),
+			// fade in when created. this could also be written as transition('void => *')
+			transition(':enter', [
+				style({ opacity: 0 }),
+				animate(400)
+			]),
 
-      // fade out when destroyed. this could also be written as transition('void => *')
-      transition(':leave',
-        animate(600, style({opacity: 0})))
-	])
-	
-  ]
+			// fade out when destroyed. this could also be written as transition('void => *')
+			transition(':leave',
+				animate(400, style({ opacity: 0 })))
+		])
+	]
 })
 export class BrandComponent implements OnInit {
+	public slides = [
+		{ image: '/assets/img/content/brand/first.jpg' }, 
+		{ image: '/assets/img/content/brand/second.jpg' },
+		{ image: '/assets/img/content/brand/tematico.jpg' }, 
+		{ image: '/assets/img/content/brand/tematico2.jpg' }];
+	public titles_brand = [
+		{ title: 'Tenemos diferentes entregas para regalo🎁' }, 
+		{ title: 'Diferentes opciones en contenido 🤔' }, 
+		{ title: 'Bandejas para los más pequeños 🤗' }, 
+		{ title: 'Hasta dinosaurios Graaaaarg 🙉' }];
+	public texts = [
+		{ text: 'Podemos entregar nuestras bandejas para tus seres queridos en sus días 🍰🎂🥳'+'.'+ 
+				'Con un contenido y adorno dependiento la ocasión que lo requiera 😎'},
+		{ text: 'No solamente ofrecemos bandejas, sino diferentes opciones y productos diferentes. Por ejemplo, plantas, dulces, conservas, y varios productos, para ti o los que quieras 🤯'},
+		{ text: 'Si quieres un desayuno para tu sobrino o hijo pequeño tenemos diferentes selecciones o adornos que te permitirán darle una sopresa maravillosa y deliciosa 👦👧🧒😋 '},
+		{ text: 'Nos hemos encontrados con fanáticos de algunas tematicas, nos importa que puedas dar un regalo dedicado y único. Aquí tenemos uno para un amiguito que le encantan los dinosaurios ! 🦕🦖'}
+	];
 
-  
-  public slides = [{'image': '/assets/img/content/brand/first.jpg'}, {'image': '/assets/img/content/brand/second.jpg'},{'image': '/assets/img/content/brand/tematico.jpg'}, {'image': '/assets/img/content/brand/tematico2.jpg'},];
-
-  private subscription: Subscription;
+	private subscription: Subscription;
 	@Output() TimerExpired: EventEmitter<any> = new EventEmitter<any>();
 	@Input() SearchDate: moment.Moment = moment();
 	@Input() ElapsTime: number = 0.10;
@@ -41,32 +54,37 @@ export class BrandComponent implements OnInit {
 	public remainingTime: number;
 	public minutes: number;
 	public seconds: number;
-	public slide : string;
-	public index : number;
-	
+	public slide: string;
+	public title_brand :string;
+	public text : string;
+	public index: number;
 
-	public everySecond: Observable<number> ;
+	public everySecond: Observable<number>;
 
 	constructor(private ref: ChangeDetectorRef) {
 		this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
 	}
 
 	ngOnInit() {
-    let everyInMinutes = this.ElapsTime * 1000 ;
-    this.index = 0;
-	this.slide = this.slides[this.index++].image;	
-    this.everySecond = timer(0, everyInMinutes);
+		let everyInMinutes = this.ElapsTime * 1000;
+		this.index = 0;
+		this.title_brand = this.titles_brand[this.index].title;
+		this.text = this.texts[this.index].text;
+		this.slide = this.slides[this.index++].image;
+		this.everySecond = timer(0,everyInMinutes); //    
 		this.subscription = this.everySecond.subscribe((seconds) => {
-			
+
 			var currentTime: moment.Moment = moment();
 			this.remainingTime = this.searchEndDate.diff(currentTime)
 			this.remainingTime = this.remainingTime / 1000;
-			
-			if (this.remainingTime == 2){
+
+			if (this.remainingTime == 2) {
 				this.slide = null;
 			}
 			if (this.remainingTime <= 0) {
-				this.slide = this.slides[this.index++].image;	
+				this.title_brand = this.titles_brand[this.index].title;
+				this.text = this.texts[this.index].text;
+				this.slide = this.slides[this.index++].image;
 			}
 			if (this.remainingTime <= 0) {
 				this.SearchDate = moment();
@@ -78,8 +96,7 @@ export class BrandComponent implements OnInit {
 				this.seconds = Math.floor(this.remainingTime - this.minutes * 60);
 			}
 			this.ref.markForCheck();
-			if (this.index == (this.slides.length) )
-			{
+			if (this.index == (this.slides.length)) {
 				this.index = 0;
 			}
 		});
